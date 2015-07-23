1e7ce9b8b617d47d1480b9acbcab016682058b54
czytelny <czytelny@gmail.com>
2015-07-23 22:14:26 +0200

    Simplifying fetchData function

--- js/bil_dev.js
+++ js/bil_dev.js
@@ -15,32 +15,26 @@
         var singleArticleList = ConfigurationHandler.getSinglesList();
         var multipleArticlesList = ConfigurationHandler.getMultipleArticlesList();
 
-        (function fetchSingleArticlesData() {
-            var curriedSingleArticleRender = function(articleName) {
-                return function(dataToRender) {
-                    renderSingleArticle(dataToRender, articleName)
-                }
-            };
-
-            for (var i = 0; i < singleArticleList.length; i++) {
-                var url = contentFolder + "/" + singleArticleList[i] + ".json";
-                RequestModule.sendGet(url, curriedSingleArticleRender(singleArticleList[i]));
+        var curriedSingleArticleRender = function(articleName) {
+            return function(dataToRender) {
+                renderSingleArticle(dataToRender, articleName)
             }
-        })();
-
-        (function fetchRepeatableData() {
-
-            var curriedRepeatableRender = function(articleName) {
-                return function(dataToRender) {
-                    renderRepeatableItems(dataToRender, articleName);
-                }
-            };
-
-            for (var i = 0; i < multipleArticlesList.length; i++) {
-                var url = contentFolder + "/" + multipleArticlesList[i] + ".json";
-                RequestModule.sendGet(url, curriedRepeatableRender(multipleArticlesList[i]));
+        };
+        var curriedRepeatableRender = function(articleName) {
+            return function(dataToRender) {
+                renderRepeatableItems(dataToRender, articleName);
             }
-        })();
+        };
+
+        sendRequestForContent(singleArticleList, curriedSingleArticleRender);
+        sendRequestForContent(multipleArticlesList, curriedRepeatableRender);
+
+        function sendRequestForContent(list, callback) {
+            for (var i = 0; i < list.length; i++) {
+                var url = contentFolder + "/" + list[i] + ".json";
+                RequestModule.sendGet(url, callback(list[i]));
+            }
+        }
     }
 
     function renderSingleArticle(data, articleName) {
@@ -67,5 +61,5 @@
     BIL.DirectivesFactory,
     BIL.ConfigurationHandler,
     BIL.CONSTANTS
-    );
+);
 
