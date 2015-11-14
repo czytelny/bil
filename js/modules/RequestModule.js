c93a9a672c701e5ba66b5d6181760d453ce141c1
czytelny <czytelny@gmail.com>
2015-11-14 23:25:27 +0100

    first play with ES6

--- js/modules/RequestModule.js
+++ js/modules/RequestModule.js
@@ -1,23 +1,22 @@
-"use strict";
-
 BIL.RequestModule = (function() {
     var REQUEST_TYPES = {
         "getRequest": "GET"
     };
-    var REQUEST_FINISHED_RESPONSE_READY = 4;
-    var OK = 200;
+
+    const REQUEST_FINISHED_RESPONSE_READY = 4;
+    const OK = 200;
 
     var hookSuccessCallback = function(dataRequest, callbackFun) {
         dataRequest.onreadystatechange = function() {
-            if (dataRequest.readyState == REQUEST_FINISHED_RESPONSE_READY && dataRequest.status == OK) {
-                var data = JSON.parse(dataRequest.responseText);
+            if (dataRequest.readyState === REQUEST_FINISHED_RESPONSE_READY && dataRequest.status === OK) {
+                let data = JSON.parse(dataRequest.responseText);
                 callbackFun(data);
             }
         };
     };
 
     var sendGet = function(url, successCallback) {
-        var dataRequest = new XMLHttpRequest();
+        let dataRequest = new XMLHttpRequest();
         hookSuccessCallback(dataRequest, successCallback);
         dataRequest.open(REQUEST_TYPES.getRequest, url, true);
         dataRequest.setRequestHeader("Content-Type", "application/json");
