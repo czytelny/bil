6705ff7f261d46e10ea9f0e284cc0db76a15577f
root <root@mic-thinkpad.home>
2015-11-21 11:43:48 +0100

    gulpfile altered to fit older node version

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,18 @@ const browserify = require('browserify');
 const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 
-gulp.task('build', () => {
-    browserify({
-        entries: 'js/test.js',
-        extensions: ['.js'],
-        debug: true
-    })
-        .transform('babelify', {presets: ['es2015']})
-        .bundle()
-        .on('error', function(err) {
-            console.error(err);
-            this.emit('end');
+gulp.task('build', function() {
+    return browserify({
+            entries: 'js/test.js',
+            extensions: ['.js'],
+            debug: true
         })
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist'));
+            .transform('babelify', {presets: ['es2015']})
+            .bundle()
+            .on('error', function(err) {
+                console.error(err);
+                this.emit('end');
+            })
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('dist'));
 });
