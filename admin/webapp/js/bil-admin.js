74e641628b3b0f780025ecdce421b467f08025df
czytelny <czytelny@gmail.com>
2015-12-15 20:16:10 +0100

    admin panel Quill editor. draft.

--- admin/webapp/js/bil-admin.js
+++ admin/webapp/js/bil-admin.js
@@ -1,1 +1,7 @@
-angular.module('BlankApp', ['ngMaterial']);
+angular.module('bil-admin', ['ngMaterial']);
+
+angular.module('bil-admin', ['ngMaterial']).controller('MainController', function(){
+  this.save = function function_name() {
+    alert(quill.getHTML());
+  }
+});
